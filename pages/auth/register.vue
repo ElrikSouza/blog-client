02aae546b90531c29ca7a0531e40f29f1ad9d7e9
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-19 17:02:29 -0400

    Created basic registration page
    Created a basic registration page. The basic api connection works, but
    the page still needs validation, styles, and error handling.

---
+++ pages/auth/register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Sign Up</h1>
+    <div>
+      <input v-model="email" type="email" placeholder="Email" />
+      <input v-model="username" type="text" placeholder="Nome de usuário" />
+      <input v-model="password" type="password" placeholder="Senha" />
+      <input
+        v-model="confirmPassword"
+        type="password"
+        placeholder="Confirmar Senha"
+      />
+
+      <button @click="submit">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    };
+  },
+  methods: {
+    async submit() {
+      if (this.password !== this.confirmPassword) {
+        alert('passwords do not match');
+        return;
+      }
+
+      const payload = {
+        email: this.email,
+        username: this.username,
+        password: this.password,
+      };
+
+      await this.$axios.post('auth/sign_up', payload);
+
+      this.$router.push('/auth/login');
+    },
+  },
+});
+</script>
