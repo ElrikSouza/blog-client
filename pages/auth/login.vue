7da08aaf79a4eb26c166836395a98cdcc6a89696
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-19 10:53:30 -0400

    Created basic login page

---
+++ pages/auth/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="grid">
+    <h1 class="grid__title">Login</h1>
+    <main>
+      <input v-model="email" type="email" placeholder="email" />
+      <input v-model="password" type="password" placeholder="password" />
+      <button @click="submit">Log In</button>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async submit() {
+      const response = await this.$axios.$post('auth/login', {
+        email: this.email,
+        password: this.password,
+      });
+
+      localStorage.setItem('token', response.token);
+      this.$router.push('/');
+    },
+  },
+});
+</script>
