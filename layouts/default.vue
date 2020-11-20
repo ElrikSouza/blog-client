7ea8abcd0fac1d1efb2851d3ed74eb55aa24a8f5
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-20 10:16:10 -0400

    Updated the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="root">
     <Nuxt />
   </div>
 </template>
@@ -15,6 +15,17 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+}
+
+html,
+body,
+.root,
+#__nuxt,
+#__layout {
+  height: 100%;
+  width: 100%;
 }
 
 *,
@@ -22,34 +33,12 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
+  padding: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+:root {
+  --primary-color: #52c876;
+  --primary-color-dark: #388a51;
+  --danger-color: #c03c14;
 }
 </style>
