c89d96db3a46676a4e0583d7819bbd9f8fb7522f
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-20 10:12:37 -0400

    Updated the register page
    Added an error list component and styles.

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,36 +1,53 @@
 <template>
-  <div>
-    <h1>Sign Up</h1>
-    <div>
-      <input v-model="email" type="email" placeholder="Email" />
-      <input v-model="username" type="text" placeholder="Nome de usuário" />
-      <input v-model="password" type="password" placeholder="Senha" />
-      <input
+  <auth-grid>
+    <auth-grid-title>Sign Up</auth-grid-title>
+    <auth-grid-form>
+      <error-list :errors="errors" />
+      <the-input v-model="email" type="email" placeholder="Email" />
+      <the-input v-model="username" type="text" placeholder="Nome de usuário" />
+      <the-input v-model="password" type="password" placeholder="Senha" />
+      <the-input
         v-model="confirmPassword"
         type="password"
         placeholder="Confirmar Senha"
       />
 
-      <button @click="submit">Sign Up</button>
-    </div>
-  </div>
+      <the-button @click="submit">Sign Up</the-button>
+    </auth-grid-form>
+  </auth-grid>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import AuthGridForm from '~/components/auth/auth-grid-form.vue';
+import AuthGridTitle from '~/components/auth/auth-grid-title.vue';
+import AuthGrid from '~/components/auth/auth-grid.vue';
+import ErrorList from '~/components/error-list.vue';
+import TheButton from '~/components/the-button.vue';
+import theInput from '~/components/the-input.vue';
+
 export default Vue.extend({
+  components: {
+    theInput,
+    TheButton,
+    AuthGrid,
+    AuthGridTitle,
+    AuthGridForm,
+    ErrorList,
+  },
   data() {
     return {
       email: '',
       username: '',
       password: '',
       confirmPassword: '',
+      errors: [] as string[],
     };
   },
   methods: {
     async submit() {
       if (this.password !== this.confirmPassword) {
-        alert('passwords do not match');
+        this.errors = ['Passwords do not match'];
         return;
       }
 
@@ -40,10 +57,33 @@ export default Vue.extend({
         password: this.password,
       };
 
-      await this.$axios.post('auth/sign_up', payload);
+      try {
+        await this.$axios.post('auth/sign_up', payload);
 
-      this.$router.push('/auth/login');
+        this.$router.push('/auth/login');
+      } catch (error) {
+        this.errors = error?.response?.data?.message ?? [
+          'Sorry, this service might be offline.',
+        ];
+      }
     },
   },
 });
 </script>
+
+<style scoped>
+.register__grid {
+  display: grid;
+  margin: auto;
+  height: 100%;
+  width: 100%;
+  max-width: 500px;
+  text-align: center;
+}
+
+.register__grid__title {
+  text-transform: uppercase;
+  font-size: 2.5rem;
+  margin: auto;
+}
+</style>
