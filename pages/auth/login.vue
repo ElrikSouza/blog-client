67e600f87bd67651baea2ee848ee90130f6a37be
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-20 10:14:03 -0400

    Updated the login page
    Added an error list component and styles.

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,32 +1,56 @@
 <template>
-  <div class="grid">
-    <h1 class="grid__title">Login</h1>
-    <main>
-      <input v-model="email" type="email" placeholder="email" />
-      <input v-model="password" type="password" placeholder="password" />
-      <button @click="submit">Log In</button>
-    </main>
-  </div>
+  <auth-grid>
+    <auth-grid-title>Login</auth-grid-title>
+    <auth-grid-form>
+      <error-list :errors="errors" />
+      <the-input v-model="email" type="email" placeholder="Email" />
+      <the-input v-model="password" type="password" placeholder="Senha" />
+
+      <the-button @click="submit">Log In</the-button>
+    </auth-grid-form>
+  </auth-grid>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import AuthGridForm from '~/components/auth/auth-grid-form.vue';
+import AuthGridTitle from '~/components/auth/auth-grid-title.vue';
+import AuthGrid from '~/components/auth/auth-grid.vue';
+import ErrorList from '~/components/error-list.vue';
+import TheButton from '~/components/the-button.vue';
+import theInput from '~/components/the-input.vue';
+
 export default Vue.extend({
+  components: {
+    theInput,
+    TheButton,
+    AuthGrid,
+    AuthGridTitle,
+    AuthGridForm,
+    ErrorList,
+  },
   data() {
     return {
       email: '',
       password: '',
+      errors: [],
     };
   },
   methods: {
     async submit() {
-      const response = await this.$axios.$post('auth/login', {
-        email: this.email,
-        password: this.password,
-      });
+      try {
+        const response = await this.$axios.$post('auth/login', {
+          email: this.email,
+          password: this.password,
+        });
 
-      localStorage.setItem('token', response.token);
-      this.$router.push('/');
+        localStorage.setItem('token', response.token);
+        this.$router.push('/posts');
+      } catch (error) {
+        this.errors = error?.response?.data?.message ?? [
+          'Sorry, this service might be offline.',
+        ];
+      }
     },
   },
 });
