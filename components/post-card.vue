ad7fe63bf36ca4fe38814e9717ca5f86974d5860
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-24 14:48:23 -0400

    Created post-card component
    This component only shows the data coming from the API. CSS will be done
    later.

---
+++ components/post-card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <nuxt-link :to="`posts/${post.user._id}`">{{
+      post.user.username
+    }}</nuxt-link>
+    <div>{{ createdAt }}</div>
+    <nuxt-link :to="`posts/${post._id}`">{{ post.title }}</nuxt-link>
+    <div class="card__body">{{ post.body }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    post: {
+      type: Object,
+      default: () => ({
+        _id: '',
+        title: 'Loading...',
+        body: 'Loading...',
+        created_at: '',
+        user: {
+          _id: 'Loading...',
+          username: 'Loading...',
+        },
+      }),
+    },
+  },
+  computed: {
+    createdAt() {
+      return new Date(this.post.created_at).toLocaleString();
+    },
+  },
+});
+</script>
